2098889ab5c6932937c4bd88765696b155383400
merryme <rj19870527@163.com>
2020-12-08 01:14:19 +0800

    提交前端架子

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [  //数组
+      'error',  //设置报错
+      {
+        semi: false, //不用分号结尾
+        singleQuote: true,
+        printWidth: 160
+      }
+    ]
+  }
+}
