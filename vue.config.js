52a66770d70c352cd1d5f81daf86c31b204951bf
merryme <rj19870527@163.com>
2020-12-08 16:59:13 +0800

    '提交scss全局变量、项目配置初始化及vue.config.js配置文件'

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
   devServer: {
     port: 8888,
     open: true
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        //引入全局文件
+        data: '@import "@/assets/scss/_variable.scss";'
+      }
+    }
   }
 }
