572ab225d17648ee9dbea0c368cb55e5652b8a64
merryme <rj19870527@163.com>
2021-02-02 02:28:26 +0800

    提交首页布局代码

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-    home
-  </div>
+  <el-row class="home" :gutter="20">
+    <!--  左边-->
+    <el-col :span="8">
+      <!--      左边上面卡片-->
+      <el-card shadow="hover" style="height: 290px">
+        鼠标悬浮时显示
+      </el-card>
+      <!--      右边上面卡片-->
+      <el-card shadow="hover" style="height: 520px;margin-top: 20px">
+        鼠标悬浮时显示
+      </el-card>
+    </el-col>
+    <!--    右边-->
+    <el-col :span="16">
+      <div class="num">
+        <el-card shadow="hover" v-for="item in 6" :key="item">
+          <i class="icon"></i>
+          <div class="detail">
+            <!--            数字-->
+            <p class="num">￥ 1234</p>
+            <!--            文本-->
+            <p class="txt">今日支付订单</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card shadow="hover">
+        <div style="height: 280px"></div>
+      </el-card>
+      <div class="graph">
+        <el-card shadow="hover">
+          <div style="height: 260px"></div>
+        </el-card>
+        <el-card shadow="hover">
+          <div style="height: 260px"></div>
+        </el-card>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -15,4 +50,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '../../assets/scss/home.scss';
+</style>
